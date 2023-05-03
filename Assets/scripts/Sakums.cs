ffa4e0f10b6bbdda4f764394248cd250c996c46b
Dzonatans <Dzonatan2017@gmail.com>
2023-05-03 10:49:28 +0300

    Main-Menu

---
+++ Assets/scripts/Sakums.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sakums : MonoBehaviour {
+
+
+    public void apturet()
+    {
+        Application.Quit();
+    }
+
+    public void uzScene()
+    {
+        SceneManager.LoadScene("izvele", LoadSceneMode.Single);
+    }
+}
