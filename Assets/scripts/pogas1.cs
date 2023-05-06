7a201491c6f1c886a0913911b81e32cef28f4614
Dzonatans <87829665+Malekitos@users.noreply.github.com>
2023-05-06 17:10:03 +0300

    pogas Funkcionalitate

---
+++ Assets/scripts/pogas1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pogas1 : MonoBehaviour {
+
+
+    public string vards;
+    public int vecums;
+    public string teksts;
+
+        public GameObject ievadesVards;
+        public GameObject ievadesVecums;
+        public GameObject tekstsParadisana;
+
+        public void uzglabaTekstu()
+        {
+
+            vards = ievadesVards.GetComponent<InputField>().text;
+            vecums = int.Parse(ievadesVecums.GetComponent<InputField>().text);
+
+            teksts = "Sikais-" + vards + ", vinam: " + vecums + " gadi";
+
+        tekstsParadisana.GetComponent<Text>().text = teksts;
+        }
+
+    }
